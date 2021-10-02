a4a279f4a3425feee5147697cedc57ebccccdb75
Joel DavidC <jdcruzzamora@gmail.com>
2021-10-02 00:04:14 -0500

    Ultima clase: video 9

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,7 +1,7 @@
 """Posts views."""
 
 #Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 #Utilities
 from datetime import datetime
@@ -9,32 +9,34 @@ from datetime import datetime
 # Create your views here.
 posts = [
 	{
-		'name': 'Mont Blac',
-		'user': 'Yesica Cortes',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=1036',
-	},
-	{
-		'name': 'Via lactea',
-		'user': 'C. Vander',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=903',
-	},
-	{
-		'name': 'Nuevo auditorio',
-		'user': 'Thespianartist',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=1076',
-	},
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
 def list_posts(request):
 	"""List existing posts."""
-	content = []
-	for post in posts:
-		content.append("""
-			<p><strong>{name}</strong></p>
-			<p><small>{user} - <i>{timestamp}</i></small></p>
-			<figure><img src="{picture}"/></figure>
-		""".format(**post))
-	return HttpResponse('<br>'.join(content))
+	return render(request, 'feed.html', {'posts': posts})
