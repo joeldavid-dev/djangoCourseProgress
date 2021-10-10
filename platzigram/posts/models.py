061183c2aad9053bf7348bd3c1e8e30939381888
Joel David C <jdcruzzamora@gmail.com>
2021-10-09 20:45:28 -0500

    Avances del video 12 (inconcluso)

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,30 @@
+"""Posts models."""
+
+#Django
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	"""User model."""
+
+	#Dato único.
+	email = models.EmailField(unique=True)
+
+	#Tamaño máximo de 100 caracteres.
+	password = models.CharField(max_length=100)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+
+	#Datos boleanos. Por defecto serán falsos.
+	is_admin = models.BooleanField(default=False)
+
+	#La biografía puede estar vacía.
+	bio = models.TextField()
+
+	#Permite que birthdate este en blanco o sea null.
+	birthdate = models.DateField(blank=True, null=True)
+
+	#Guarda automáticamente la fecha de creación.
+	create = models.DateTimeField(auto_now_add=True)
+
+	#Guarda automáticamente la fecha de edición.
+	modified = models.DateTimeField(auto_now=True)
